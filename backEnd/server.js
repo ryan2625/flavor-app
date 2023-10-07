c14b97278c633eeaf5356bfb96f817047d59fb34
ryan2625 <ryanfreas19@augustana.edu>
2023-10-07 12:29:46 -0500

    Adding CORS and deployment modules

--- backEnd/server.js
+++ backEnd/server.js
@@ -4,6 +4,7 @@ const app = express();
 const dotenv = require('dotenv');
 const flavorRoutes = require('./routes/flavors');
 const quoteRoutes = require('./routes/quotes');
+const cors = require('cors');
 
 /**
  * Server Configuration and Routes
@@ -17,11 +18,10 @@ dotenv.config();
 // Enable JSON request body parsing
 app.use(express.json());
 
-// Configure CORS headers so we can allow frontend on port 3000 to access our port 4000 backend
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "true");
 });
 
 app.use('/api/flavors', flavorRoutes);
